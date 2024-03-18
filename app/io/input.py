bfb8b986493ae9a1127dd4338d0af196ebfee1f8
AndriiKviatkovskyi <133859288+AndriiKviatkovskyi@users.noreply.github.com>
2024-03-18 17:56:32 +0200

    Added blank methods

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+import pandas
+
+
+def read_input():
+    return;
+
+
+def read_file_python():
+    return;
+
+
+def read_file_pandas():
+    return;
---
+++ app/io/output.py
@@ -0,0 +1,9 @@
+import pandas
+
+
+def print_output():
+    return;
+
+
+def write_file_pandas():
+    return;
